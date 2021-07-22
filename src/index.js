f502f8a14ea43a2a427424e352f91cc05708155c
dsaucez <damien.saucez@gmail.com>
2021-07-22 14:00:07 +0000

    provide server identifier

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
+const pid = process.pid;
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/time', (req,res) => { res.send({time: new Date()}) });
+app.get('/time', (req,res) => { res.send({time: new Date(), id: pid}) });
 
-app.listen(PORT, ()=> {console.log('Time server is listening on port '+ PORT)});
+app.listen(PORT, ()=> {console.log('Time server ' + pid + ' is listening on port '+ PORT)});
